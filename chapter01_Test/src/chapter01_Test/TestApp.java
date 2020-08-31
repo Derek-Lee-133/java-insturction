fd5f36aa1552561b74e76e05a14270f7dbdd51f5
Derek Lee <dlee133@gmail.com>
2020-08-31 18:25:36 -0400

    init

---
+++ chapter01_Test/src/chapter01_Test/TestApp.java
@@ -0,0 +1,13 @@
+package chapter01_Test;
+
+public class TestApp {
+
+	 void main(String[] args) {
+		System.out.println("Success!");
+		System.out.println("Success!");
+		System.out.println("Success!");
+		// TODO 
+
+	}
+
+}
