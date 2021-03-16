69b36d2dd2fcc6a19264ad95717b5e1a4ac35a30
Derek Lee <dlee133@gmail.com>
2021-03-15 23:10:03 -0400

    code challengeish

--- sds-inn-inventory/src/Item.java
+++ sds-inn-inventory/src/Item.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Item {
 
@@ -43,8 +44,87 @@ public class Item {
 	// printing array of Item
 	@Override
 	public String toString() {
-		return String.format("Inventory" + "\n" + "Name: " + Name + "\n" +
-	"Sellin: " + SellIn + "\n"+ "Quality: " + Quality + "\n");
+		return String.format("Inventory" + "\n" + "Name: " + Name + "\n" + "Sellin: " + SellIn + "\n" + "Quality: "
+				+ Quality + "\n");
+	}
+
+	public static void  updateQuality(ArrayList Items) {
+		Items = Items[];
+		
+		for (Items i = 0; i < Items.Count; i++)
+		{
+			if (Items[i].name = "Conjured") {
+				Items[i].Quality = Items[i].Quality -2;
+			}
+		
+			if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage Passes")
+		{
+				if (Items[i].Quality > 0)
+		{
+		if (Items[i].Name != "Sulfuras")
+		{
+		Items[i].Quality = Items[i].Quality - 1;
+		}
+		
+		}
+		}
+		else
+		{
+		if (Items[i].Quality < 50)
+		{
+		Items[i].Quality = Items[i].Quality + 1;
+		if (Items[i].Name == "Backstage Passes")
+		{
+		if (Items[i].SellIn < 11)
+		{
+		if (Items[i].Quality < 50)
+		{
+		Items[i].Quality = Items[i].Quality + 1;
+		}
+		}
+
 	}
 
 }
+		if (Items[i].SellIn < 6)
+		{
+			if (Items[i].Quality < 50)
+			{
+			Items[i].Quality = Items[i].Quality + 1;
+			}
+			 }
+			}
+			}
+			
+			if (Items[i].Name != "Sulfuras")
+			{
+			Items[i].SellIn = Items[i].SellIn - 1;
+			}
+			if (Items[i].SellIn < 0)
+			{
+			if (Items[i].Name != "Aged Brie")
+			{
+			if (Items[i].Name != "Backstage passes")
+			{
+			if (Items[i].Quality > 0)
+			{
+			if (Items[i].Name != "Sulfuras")
+			{
+			Items[i].Quality = Items[i].Quality - 1;
+			}
+			}
+			}
+			else
+			{
+			Items[i].Quality = Items[i].Quality - Items[i].Quality;
+		}
+			}
+			else
+			{
+			if (Items[i].Quality < 50)
+			{
+			Items[i].Quality = Items[i].Quality + 1;
+			}
+			}
+			}
+	}
